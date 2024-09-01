4a6fb8b616bf8d2c6b570e128b7fa5c9663b2250
Miran-Mirantee <killerm11805@hotmail.com>
2024-09-02 03:23:22 +0700

    implement basic audio player

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,59 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const formatTime = (seconds: number) => {
+  const minutes = Math.floor(seconds / 60);
+  const secs = Math.floor(seconds % 60);
+  return `${minutes.toString().padStart(2, "0")}:${secs
+    .toString()
+    .padStart(2, "0")}`;
+};
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// const bodyDom = document.body;
+const audio = new Audio("./music/Kagami.mp3");
+
+// const audioDuration =
+
+const playBtn = document.querySelector(".play");
+playBtn?.addEventListener("click", () => {
+  audio.play();
+  if (durationDom) {
+    durationDom.textContent = formatTime(audio.duration);
+  }
+});
+
+const pauseBtn = document.querySelector(".pause");
+pauseBtn?.addEventListener("click", () => {
+  audio.pause();
+});
+
+const currentTimeDom = document.querySelector(".time");
+const durationDom = document.querySelector(".maxTime");
+
+audio.addEventListener("timeupdate", () => {
+  if (currentTimeDom) {
+    currentTimeDom.textContent = formatTime(audio.currentTime);
+  }
+  if (seekBar) {
+    seekBar.value = (audio.currentTime / audio.duration).toString();
+  }
+});
+
+const volumnRange = document.querySelector(".volumn") as HTMLInputElement;
+
+(() => {
+  audio.volume = parseFloat(volumnRange.value);
+})();
+
+volumnRange?.addEventListener("input", (e: any) => {
+  audio.volume = e.target.value;
+});
+
+const seekBar = document.querySelector(".seekBar") as HTMLInputElement;
+
+seekBar?.addEventListener("input", (e: any) => {
+  const seekTo = audio.duration * e.target.value;
+  console.log(seekTo);
+  if (audio) {
+    audio.currentTime = seekTo;
+  }
+});
